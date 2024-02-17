ccf72150618f31f121323adcdd0d65caa67e70ee
a1909330 <159606975+a1909330@users.noreply.github.com>
2024-02-17 17:44:22 +1030

    Update question12.sh

--- question12.sh
+++ question12.sh
@@ -1,5 +1,15 @@
+
 #!/bin/bash
 
+# Switch back to branch2
 git checkout branch2
+
+# Restore the uncommitted changes
 git stash pop
-git commit -m "Committed modified content in file4"
+
+# Commit the restored changes
+git add file4
+git commit -m "Restore and commit uncommitted changes"
+
+# Verify the commit
+git log
